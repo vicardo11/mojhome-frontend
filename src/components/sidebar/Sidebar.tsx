7d48ff614496b1680804b6bd5a7339aa38af6733
Patryk Sosinski <patryk.sosinski@lhsystems.com>
2024-02-17 11:24:38 +0100

    Add icon and logo

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "./Sidebar.scss";
 import { Link } from "react-router-dom";
 import { SidebarData } from "./SidebarData";
+import { LogoIcon } from "../logoIcon/LogoIcon";
 
 const Sidebar = () => {
   return (
     <nav className="nav-menu">
+      <div className="brand">
+        <LogoIcon />
+      </div>
       <ul className="nav-menu-items">
         {SidebarData.map((item, index) => {
           const Icon = item.icon;
