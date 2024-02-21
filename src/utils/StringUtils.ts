bc9275f6fab4a3c684c5d12a697dc3e152f37332
Patryk Sosinski <patryk.sosinski@lhsystems.com>
2024-02-21 18:52:52 +0100

    String isEmpty function

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
+
+export function isEmpty(str: string | undefined): boolean {
+  return !str || str.trim() === "";
+}
