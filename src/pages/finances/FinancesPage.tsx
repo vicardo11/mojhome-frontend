8d4b4d9c38ffdf8e33f015049c2367fc2237f0f4
Patryk Sosinski <patryk.sosinski@lhsystems.com>
2024-02-29 20:32:00 +0100

    Handle errors on FinancePage

--- src/pages/finances/FinancesPage.tsx
+++ src/pages/finances/FinancesPage.tsx
@@ -1,4 +1,3 @@
-import Box from "@mui/material/Box";
 import React, { useEffect, useState } from "react";
 import CTable from "../../components/table/CTable";
 import useAxios from "../../hooks/useAxios";
@@ -9,18 +8,26 @@ import { IoIosAddCircleOutline } from "react-icons/io";
 import CIconButton from "../../components/icon-button/CIconButton";
 import { COLOR_BLUE } from "../../constants/Colors";
 import { FinanceService } from "./service/FinanceService";
+import { Alert, Box } from "@mui/material";
 
 const FinancesPage = () => {
   const [selectedFinanceRecord, setSelectedFinanceRecord] = useState<FinanceRecord>();
   const [isEditFinanceModalOpen, setIsEditFinanceModalOpen] = useState(false);
   const [financeRecords, setFinanceRecords] = useState<FinanceRecord[]>([]);
+  const [error, setError] = useState<string | null>(null);
   const axios = useAxios();
   const financeService = new FinanceService(axios);
 
   useEffect(() => {
-    financeService.getFinances().then((response) => {
-      setFinanceRecords(response.data);
-    });
+    financeService
+      .getFinances()
+      .then((response) => {
+        setFinanceRecords(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        setError(error.response.data.message);
+      });
   }, []);
 
   function handleRowSelected(id: string) {
@@ -36,16 +43,26 @@ const FinancesPage = () => {
   function handleEditFinanceModalSubmitted(record: FinanceRecord) {
     // If ID === null then create else update
     record.id
-      ? financeService.updateFinance(record).then((response) => {
-          const newFinanceRecords = financeRecords.map((financeRecord) => {
-            return financeRecord.id === response.data.id ? record : financeRecord;
+      ? financeService
+          .updateFinance(record)
+          .then((response) => {
+            const newFinanceRecords = financeRecords.map((financeRecord) => {
+              return financeRecord.id === response.data.id ? record : financeRecord;
+            });
+            setFinanceRecords(newFinanceRecords);
+          })
+          .catch((error) => {
+            setError(error.response.data.message);
+          })
+      : financeService
+          .createFinance(record)
+          .then((response) => {
+            const newFinanceRecords = [...financeRecords, response.data];
+            setFinanceRecords(newFinanceRecords);
+          })
+          .catch((error) => {
+            setError(error.response.data.message);
           });
-          setFinanceRecords(newFinanceRecords);
-        })
-      : financeService.createFinance(record).then((response) => {
-          const newFinanceRecords = [...financeRecords, response.data];
-          setFinanceRecords(newFinanceRecords);
-        });
     handleEditFinanceModalClosed();
   }
 
@@ -55,6 +72,9 @@ const FinancesPage = () => {
 
   return (
     <Box>
+      <Alert sx={{ mb: 3, display: error || "none" }} severity="error">
+        {error}
+      </Alert>
       <CTable
         title="Finances"
         data={financeRecords}
