7af0735b87067769d0e2b232ea487ea56717bada
Patryk Sosinski <patryk.sosinski@lhsystems.com>
2024-03-11 20:04:27 +0100

    Adapted new colors

--- src/constants/Colors.ts
+++ src/constants/Colors.ts
@@ -1,28 +1,21 @@
 export const COLOR_GREEN = "#369338";
-
 export const COLOR_BLUE = "#004aad";
-
-export const COLOR_RED = "rgba(215,28,28,0.82)";
+export const COLOR_RED = "#D71C1CD1";
+export const COLOR_ORANGE = "#ff7f0e";
+export const COLORS_PURPLE = "#9467bd";
+export const COLOR_BROWN = "#8c564b";
+export const COLOR_PINK = "#e377c2";
+export const COLOR_GREY = "#7f7f7f";
+export const COLOR_YELLOW = "#bcbd22";
 
 export const PIE_CHART_COLORS = [
-  "#FF6633",
-  "#FFB399",
-  "#FF33FF",
-  "#FFFF99",
-  "#00B3E6",
-  "#E6B333",
-  "#3366E6",
-  "#999966",
-  "#99FF99",
-  "#B34D4D",
-  "#80B300",
-  "#809900",
-  "#E6B3B3",
-  "#6680B3",
-  "#66991A",
-  "#FF99E6",
-  "#CCFF1A",
-  "#FF1A66",
-  "#E6331A",
-  "#33FFCC",
+  COLOR_BLUE,
+  COLOR_ORANGE,
+  COLOR_GREEN,
+  COLOR_RED,
+  COLORS_PURPLE,
+  COLOR_BROWN,
+  COLOR_PINK,
+  COLOR_GREY,
+  COLOR_YELLOW,
 ];
