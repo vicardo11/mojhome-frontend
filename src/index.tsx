e08704be4a796537fb20d4e80c7f2f38d0fe8bcd
Patryk Sosinski <patryk.sosinski@lhsystems.com>
2024-04-16 17:01:32 +0200

    Inject all firebase variables to CI/CD

--- src/index.tsx
+++ src/index.tsx
@@ -17,9 +17,9 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_VERCEL_API_KEY,
   authDomain: process.env.REACT_APP_VERCEL_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_VERCEL_PROJECT_ID,
-  storageBucket: "mojhome-f0f0a.appspot.com",
-  messagingSenderId: "626806674671",
-  appId: "1:626806674671:web:60cebc05d7e28899d7b71a",
+  storageBucket: process.env.REACT_APP_VERCEL_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_VERCEL_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_VERCEL_APP_ID,
 };
 
 // Initialize Firebase
